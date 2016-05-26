d1a1c54a9956d772cd3f9bcd797dcfb79c65b040
hdw542 <hdw542@hotmail.com>
2016-05-26 20:58:23 +0500

    Vp Project

---
+++ VP/TY.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace VP
+{
+    partial class TY
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(86, 266);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(75, 23);
+            this.btnEnable.TabIndex = 0;
+            this.btnEnable.Text = "Enable";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Location = new System.Drawing.Point(189, 266);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(75, 23);
+            this.btnDisable.TabIndex = 1;
+            this.btnDisable.Text = "Disable";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(434, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // TY
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::VP.Properties.Resources._350260;
+            this.ClientSize = new System.Drawing.Size(347, 321);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Name = "TY";
+            this.Text = "TY Artificial Intelligence";
+            this.Load += new System.EventHandler(this.TY_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VP/TY.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+namespace VP
+{
+    public partial class TY : Form
+    {
+
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+
+        SpeechSynthesizer spSynthesizer = new SpeechSynthesizer();
+
+        public TY()
+        {
+            InitializeComponent();  
+            Choices commands = new Choices();
+            var lines = System.IO.File.ReadAllLines(@"D:\Hadi.txt");
+            commands.Add(lines);
+            GrammarBuilder gramBuilder = new GrammarBuilder();
+            gramBuilder.Append(commands);
+            
+            Grammar grammar = new Grammar(gramBuilder);
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+            btnEnable.Enabled = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static string[] ReadFile()
+        {
+            string[] lines = File.ReadAllLines(@"D:\Hadi.txt", Encoding.UTF8);
+            return lines;
+        }
+
+        void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string[] Data = ReadFile();
+            string question = "";
+            string answer = "";
+            for (int i = 0; i < Data.Length; i++)
+            {
+                if (Data[i]==e.Result.Text)
+                {
+                    question = Data[i];
+                    answer = Data[i + 1];
+                    spSynthesizer.SpeakAsync(answer);
+                }
+            }
+           
+                    
+              
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+            btnEnable.Enabled = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(@"D:\Hadi.txt", Encoding.UTF8);
+           
+        }
+
+        private void TY_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
